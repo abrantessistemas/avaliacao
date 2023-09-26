ede69a344ebb501f37f430cd6145edf56a999438
Adriano A. <abrantessistemas@gmail.com>
2023-09-26 15:46:45 -0300

    Added, change image and adjustment in the genre field, title field and added action buttons

--- src/app/pages/users/users-create-update/users-create-update.component.ts
+++ src/app/pages/users/users-create-update/users-create-update.component.ts
@@ -1,7 +1,12 @@
 import { Component, Inject } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Subscription } from 'rxjs';
 import { UserModel } from '../model/user.model';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UserService } from './../model/user.service';
+import { BadRequestContract } from 'src/app/common/bad-request-contract.model';
+import { ConfirmationDialogComponent } from 'src/app/common/dialog/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-users-create-update',
@@ -10,23 +15,31 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 })
 export class UsersCreateUpdateComponent {
   userForm!: FormGroup;
+  mode: 'delete' | 'create' | 'update' = 'create';
+  listTitle = ['mr', 'ms', 'mrs', 'miss', 'dr'];
+  listGender = ['male', 'female', 'other'];
 
-  constructor(@Inject(MAT_DIALOG_DATA) public defaults: UserModel, private fb: FormBuilder) { }
+  private subscription: Subscription = new Subscription();
+
+  constructor(@Inject(MAT_DIALOG_DATA) public defaults: UserModel, private fb: FormBuilder,
+    private dialogRef: MatDialogRef<UsersCreateUpdateComponent>, private dialog: MatDialog,
+    private snackbar: MatSnackBar, private userService: UserService) { }
 
   ngOnInit() {
     if (this.defaults) {
-
+      this.mode = 'update';
     } else {
       this.defaults = {} as UserModel;
     }
 
     this.userForm = this.fb.group({
+      id: this.defaults.id || '',
       firstName: this.defaults.firstName || '',
       lastName: this.defaults.lastName || '',
       title: this.defaults.title || '',
       gender: this.defaults.gender || '',
-      email: this.defaults.email || '',
-      dateOfBirth: new Date (this.defaults.dateOfBirth) || '',
+      email: [this.defaults.email, Validators.email] || '',
+      dateOfBirth: this.defaults.dateOfBirth || '',
       registerDate: this.defaults.registerDate || '',
       phone: this.defaults.phone || '',
       picture: this.defaults.picture || '',
@@ -34,5 +47,106 @@ export class UsersCreateUpdateComponent {
     });
 
   }
-  save() { }
+  save() {
+    if (this.mode === 'create') {
+      this.createUser();
+    } else if (this.mode === 'update') {
+      this.updateUser();
+    }
+  }
+
+  isCreateMode() {
+    return this.mode === 'create';
+  }
+
+  isUpdateMode() {
+    return this.mode === 'update';
+  }
+
+  createUser() {
+    const user = this.userForm.value;
+
+    this.subscription.add(
+      this.userService.create(user).subscribe(
+        (result) => {
+          this.snackbar.open(
+            'User ' +
+            result +
+            ' successfully created.',
+            'OK',
+            {
+              duration: 5000,
+            }
+          );
+
+          this.dialogRef.close(user);
+        },
+        (exception: BadRequestContract) => {
+          console.log(exception.data)
+          this.snackbar.open(exception.message, 'ERROR', {
+            duration: 5000,
+          });
+        }
+      )
+    );
+  }
+
+  updateUser() {
+    const user: UserModel = this.userForm.value;
+
+    this.subscription.add(
+      this.userService.update(user.id, user).subscribe(
+        (result) => {
+          this.snackbar.open(
+            'User ' +
+            result +
+            ' updated successfully.',
+            'OK',
+            {
+              duration: 5000,
+            }
+          );
+
+          this.dialogRef.close(user);
+        },
+        (exception: BadRequestContract) => {
+          console.log(exception.data)
+          this.snackbar.open(exception.message, 'ERROR', {
+            duration: 5000,
+          });
+        }
+      )
+    );
+  }
+
+  deleteUser() {
+    const user: UserModel = this.userForm.value;
+
+    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
+      data: {
+        message: 'Are you sure you want to delete user ' + user.firstName + '?'
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.userService.delete(user.id).subscribe(() => {
+          this.snackbar.open('Item deleted successfully.', 'Close', {
+            duration: 5 * 1000,
+          });
+        });
+      }
+      this.dialogRef.close(user);
+    });
+  }
+
+  pictureChange = false;
+
+  updateImage() {
+    this.pictureChange = !this.pictureChange;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
