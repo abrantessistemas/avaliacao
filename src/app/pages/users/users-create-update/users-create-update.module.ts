655af186aab5216a695d9d32a9defad38e192069
Adriano A. <abrantessistemas@gmail.com>
2023-09-26 23:32:19 -0300

    add pipe module

--- src/app/pages/users/users-create-update/users-create-update.module.ts
+++ src/app/pages/users/users-create-update/users-create-update.module.ts
@@ -1,17 +1,18 @@
-import { MatTabsModule } from '@angular/material/tabs';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { UsersCreateUpdateComponent } from './users-create-update.component';
-import { MatButtonModule } from '@angular/material/button';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { PipesModule } from 'src/app/pipes/pipes.module';
+import { UsersCreateUpdateComponent } from './users-create-update.component';
 
 
 @NgModule({
@@ -30,7 +31,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatDatepickerModule,
     MatNativeDateModule,
     MatSelectModule,
-    MatTabsModule
+    MatTabsModule,
+    PipesModule
   ]
 })
 export class UsersCreateUpdateModule { }
